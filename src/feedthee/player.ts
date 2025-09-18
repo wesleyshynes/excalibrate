73b6eec669457cb7e241fac9e21ac8d92e0832dc
wesleyshynes <wesleyshynes@gmail.com>
2025-09-17 21:04:39 -0400

    big animation

--- src/feedthee/player.ts
+++ src/feedthee/player.ts
@@ -10,7 +10,6 @@ import {
     Keys,
     Label,
     Side,
-    Sprite,
     SpriteSheet,
     TextAlign,
     vec,
@@ -20,18 +19,11 @@ import { Resources } from "./resources";
 export class Player extends Actor {
 
     lastSpeed = vec(0, 0);
+    lastDirection = 'down';
 
     playerLabel: Label | undefined;
 
     engineRef: Engine | undefined;
-
-    // idleSprite!: Sprite;
-    // walkSprite!: Sprite;
-    // runSprite!: Sprite;
-
-    // idleAnimation!: Animation;
-    // walkAnimation!: Animation
-    // runAnimation!: Animation
 
     constructor(name: string = 'player') {
         super({
@@ -65,6 +57,9 @@ export class Player extends Actor {
         this.updatePlayerLabel('Player');
         this.addChild(playerLabel);
 
+        const animationSpeed = 60
+
+        // IDLE ANIMATIONS
         const idleSpriteSheet = SpriteSheet.fromImageSource({
             image: Resources.PlayerIdle,
             grid: {
@@ -75,7 +70,6 @@ export class Player extends Actor {
             },
 
         });
-        const animationSpeed = 60
 
         const idleAnimationDown = Animation.fromSpriteSheet(
             idleSpriteSheet,
@@ -91,6 +85,7 @@ export class Player extends Actor {
             animationSpeed // Frame duration in milliseconds
         );
         this.graphics.add('idle-right-down', idleAnimationRightDown);
+        this.graphics.add('idle-left-down', idleAnimationRightDown); // Use the same animation for left, will flip horizontally
 
         const idleAnimationRight = Animation.fromSpriteSheet(
             idleSpriteSheet,
@@ -98,6 +93,7 @@ export class Player extends Actor {
             animationSpeed // Frame duration in milliseconds
         );
         this.graphics.add('idle-right', idleAnimationRight);
+        this.graphics.add('idle-left', idleAnimationRight); // Use the same animation for left, will flip horizontally
 
         const idleAnimationRightUp = Animation.fromSpriteSheet(
             idleSpriteSheet,
@@ -105,6 +101,7 @@ export class Player extends Actor {
             animationSpeed // Frame duration in milliseconds
         );
         this.graphics.add('idle-right-up', idleAnimationRightUp);
+        this.graphics.add('idle-left-up', idleAnimationRightUp); // Use the same animation for left, will flip horizontally
 
         const idleAnimationUp = Animation.fromSpriteSheet(
             idleSpriteSheet,
@@ -113,8 +110,102 @@ export class Player extends Actor {
         );
         this.graphics.add('idle-up', idleAnimationUp);
 
+        // WALK ANIMATIONS
+        const walkSpriteSheet = SpriteSheet.fromImageSource({
+            image: Resources.PlayerWalk,
+            grid: {
+                rows: 5,
+                columns: 4,
+                spriteWidth: 20,
+                spriteHeight: 19,
+            },
 
+        });
+        const walkAnimationDown = Animation.fromSpriteSheet(
+            walkSpriteSheet,
+            [0, 1, 2, 3], // All frames in order
+            animationSpeed // Frame duration in milliseconds
+        );
+        this.graphics.add('walk-down', walkAnimationDown);
 
+        const walkAnimationRightDown = Animation.fromSpriteSheet(
+            walkSpriteSheet,
+            [4, 5, 6, 7], // All frames in order
+            animationSpeed // Frame duration in milliseconds
+        );
+        this.graphics.add('walk-right-down', walkAnimationRightDown);
+        this.graphics.add('walk-left-down', walkAnimationRightDown); // Use the same animation for left, will flip horizontally
+
+        const walkAnimationRight = Animation.fromSpriteSheet(
+            walkSpriteSheet,
+            [8, 9, 10, 11], // All frames in order
+            animationSpeed // Frame duration in milliseconds
+        );
+        this.graphics.add('walk-right', walkAnimationRight);
+        this.graphics.add('walk-left', walkAnimationRight); // Use the same animation for left, will flip horizontally
+
+        const walkAnimationRightUp = Animation.fromSpriteSheet(
+            walkSpriteSheet,
+            [12, 13, 14, 15], // All frames in order
+            animationSpeed // Frame duration in milliseconds
+        );
+        this.graphics.add('walk-right-up', walkAnimationRightUp);
+        this.graphics.add('walk-left-up', walkAnimationRightUp); // Use the same animation for left, will flip horizontally
+
+        const walkAnimationUp = Animation.fromSpriteSheet(
+            walkSpriteSheet,
+            [16, 17, 18, 19], // All frames in order
+            animationSpeed // Frame duration in milliseconds
+        );
+        this.graphics.add('walk-up', walkAnimationUp);
+
+        // RUN ANIMATIONS
+        const runSpriteSheet = SpriteSheet.fromImageSource({
+            image: Resources.PlayerRun,
+            grid: {
+                rows: 5,
+                columns: 6,
+                spriteWidth: 20,
+                spriteHeight: 20,
+            },
+        });
+        const runAnimationDown = Animation.fromSpriteSheet(
+            runSpriteSheet,
+            [0, 1, 2, 3, 4, 5], // All frames in order
+            animationSpeed // Frame duration in milliseconds
+        );
+        this.graphics.add('run-down', runAnimationDown);
+
+        const runAnimationRightDown = Animation.fromSpriteSheet(
+            runSpriteSheet,
+            [6, 7, 8, 9, 10, 11], // All frames in order
+            animationSpeed // Frame duration in milliseconds
+        );
+        this.graphics.add('run-right-down', runAnimationRightDown);
+        this.graphics.add('run-left-down', runAnimationRightDown); // Use the same animation for left, will flip horizontally
+
+        const runAnimationRight = Animation.fromSpriteSheet(
+            runSpriteSheet,
+            [12, 13, 14, 15, 16, 17], // All frames in order
+            animationSpeed // Frame duration in milliseconds
+        );
+        this.graphics.add('run-right', runAnimationRight);
+        this.graphics.add('run-left', runAnimationRight); // Use the same animation for left, will flip horizontally
+
+        const runAnimationRightUp = Animation.fromSpriteSheet(
+            runSpriteSheet,
+            [18, 19, 20, 21, 22, 23], // All frames in order
+            animationSpeed // Frame duration in milliseconds
+        );
+        this.graphics.add('run-right-up', runAnimationRightUp);
+        this.graphics.add('run-left-up', runAnimationRightUp); // Use the same animation for left, will flip horizontally
+
+        const runAnimationUp = Animation.fromSpriteSheet(
+            runSpriteSheet,
+            [24, 25, 26, 27, 28, 29], // All frames in order
+            animationSpeed // Frame duration in milliseconds
+        );
+        this.graphics.add('run-up', runAnimationUp);
     }
 
     updatePlayerLabel(text: string) {
@@ -143,31 +234,36 @@ export class Player extends Actor {
         let moveX = false
         let moveY = false
 
+        const accelerationIncrement = 5;
+
+        let xDirection = ''
+        let yDirection = ''
+
         if (
             engine.input.keyboard.isHeld(Keys.W) ||
             engine.input.keyboard.isHeld(Keys.Up)
         ) {
-            this.vel.y += -10; // Move up
+            this.vel.y += -accelerationIncrement; // Move up
             moveY = true;
-            this.graphics.use('idle-up');
+            yDirection = 'up'
         }
 
         if (
             engine.input.keyboard.isHeld(Keys.S) ||
             engine.input.keyboard.isHeld(Keys.Down)
         ) {
-            this.vel.y += 10; // Move down
+            this.vel.y += accelerationIncrement; // Move down
             moveY = true;
-            this.graphics.use('idle-down');
+            yDirection = 'down'
         }
 
         if (
             engine.input.keyboard.isHeld(Keys.A) ||
             engine.input.keyboard.isHeld(Keys.Left)
         ) {
-            this.vel.x += -10; // Move left
+            this.vel.x += -accelerationIncrement; // Move left
             moveX = true;
-            this.graphics.use('idle-right');
+            xDirection = 'left'
             this.graphics.flipHorizontal = false;
         }
 
@@ -175,14 +271,19 @@ export class Player extends Actor {
             engine.input.keyboard.isHeld(Keys.D) ||
             engine.input.keyboard.isHeld(Keys.Right)
         ) {
-            this.vel.x += 10; // Move right
+            this.vel.x += accelerationIncrement; // Move right
             moveX = true;
-            this.graphics.use('idle-right');
+            xDirection = 'right'
             this.graphics.flipHorizontal = true;
         }
 
         let maxVelocity = 400;
         // const maxSpeed = Math.sqrt(this.vel.x * this.vel.x + this.vel.y * this.vel.y);
+
+        // if holding shift cap the max speed
+        if (engine.input.keyboard.isHeld(Keys.ShiftRight) || engine.input.keyboard.isHeld(Keys.ShiftLeft)) {
+            maxVelocity = 200;
+        }
 
         const absoluteVelocity = Math.sqrt(this.vel.x * this.vel.x + this.vel.y * this.vel.y);
         if (absoluteVelocity > maxVelocity) {
@@ -190,6 +291,33 @@ export class Player extends Actor {
             this.vel.x *= scale;
             this.vel.y *= scale; // Scale down the velocity to maxVelocity
         }
+
+        let animationStringBase = 'idle';
+        if (absoluteVelocity > 220) {
+            animationStringBase = 'run';
+        } else if (absoluteVelocity > 20) {
+            animationStringBase = 'walk';
+        } else {
+            animationStringBase = 'idle';
+        }
+
+        let animationDirection = '';
+        if (xDirection) {
+            animationDirection = xDirection;
+        }
+        if (yDirection) {
+            animationDirection = animationDirection ? (animationDirection + '-' + yDirection) : yDirection;
+        }
+
+        if (animationDirection) {
+            // If there is a direction change, use the new direction
+            this.lastDirection = animationDirection;
+        }
+
+        const animationToUse = animationStringBase + '-' + this.lastDirection;
+        this.graphics.use(animationToUse);
+
+        this.updatePlayerLabel(Math.floor(absoluteVelocity) + ' px/s');
 
         // this.vel.x = clamp(this.vel.x, -400, 400); // Limit horizontal speed
         // this.vel.y = clamp(this.vel.y, -400, 400); // Limit vertical speed
